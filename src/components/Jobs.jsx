a4315e64ababb0178cbc80ed41eec182babe5025
Alvaro <alvaro.martin.crespo.00@gmail.com>
2023-06-20 18:33:06 +0200

    jobs (it has to be update)

--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -6,7 +6,13 @@ export default function Jobs() {
         Jobs
       </h2>
       <div className="text-white flex justify-center mt-10">
-        <Tabs defaultTab="tab-1" color="gray" radius="lg" tone="pill">
+        <Tabs
+          className="flex justify-center flex-col items-center"
+          defaultTab="tab-1"
+          color="gray"
+          radius="lg"
+          tone="pill"
+        >
           <Tabs.List>
             <Tabs.Tab anchor="tab-1">DSS NetWork</Tabs.Tab>
             <Tabs.Tab anchor="tab-2">Personal projects</Tabs.Tab>
@@ -14,14 +20,33 @@ export default function Jobs() {
 
           <Tabs.Content anchor="tab-1">
             <div className="flex justify-center flex-col items-center">
-              <h2 className="text-2xl font-bold">DSS NetWork</h2>
-              <p>Info about job</p>
+              <h2 className="text-2xl font-bold">DSS Network</h2>
+              <span className="mt-2 text-xs italic font-bold">"HTML, CSS, JavaScript"</span>
+              <div className="grid grid-cols-1 md:grid-cols-2 my-10 items-center">
+                <p className="text-center p-5">
+                  My first job involved dedicating time to web design using HTML and CSS (Bootstrap). I utilized
+                  JavaScript (jQuery) for some animations and page functions, including libraries, as well as working
+                  with PHP.
+                </p>
+                <img className="p-10" src="img/dssnetwork.png" alt="dssnetwork" />
+              </div>
             </div>
           </Tabs.Content>
           <Tabs.Content anchor="tab-2">
             <div className="flex justify-center flex-col items-center">
               <h2 className="text-2xl font-bold">Personal Projects</h2>
-              <p>Info about my personal projects</p>
+              <span className="mt-2 text-xs italic font-bold">"ReactJS, NextJS, MongoDB, ..."</span>
+              <p className="my-5 px-16 text-center">
+                I am a technology enthusiast who is always eager to learn new technologies. I have completed several
+                projects in ReactJS and I am currently learning NextJS to have a full-stack profile. I am passionate
+                about front-end development with JavaScript and its various frameworks.
+              </p>
+              <a
+                className="text-amber-400 font-bold py-2 px-4 rounded-lg shadow-md hover:bg-slate-800"
+                href="https://github.com/AlvaroMartinCrespo"
+              >
+                Visit GitHub
+              </a>
             </div>
           </Tabs.Content>
         </Tabs>
