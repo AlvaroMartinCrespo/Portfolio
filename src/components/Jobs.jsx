5481f7bf3baf5ff263a3d206bd3c82b1e524eb32
Alvaro <alvaro.martin.crespo.00@gmail.com>
2023-06-20 18:06:23 +0200

    udpate

--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -1,10 +1,31 @@
+import { Tabs } from '@rewind-ui/core';
 export default function Jobs() {
   return (
     <section id="jobs" className="container mx-auto my-10">
       <h2 className="text-5xl text-center font-bold bg-gradient-to-r from-gray-100 to-gray-700 text-transparent bg-clip-text underline-offset-8 underline decoration-2 decoration-amber-400">
         Jobs
       </h2>
-      <div></div>
+      <div className="text-white flex justify-center mt-10">
+        <Tabs defaultTab="tab-1" color="gray" radius="lg" tone="pill">
+          <Tabs.List>
+            <Tabs.Tab anchor="tab-1">DSS NetWork</Tabs.Tab>
+            <Tabs.Tab anchor="tab-2">Personal projects</Tabs.Tab>
+          </Tabs.List>
+
+          <Tabs.Content anchor="tab-1">
+            <div className="flex justify-center flex-col items-center">
+              <h2 className="text-2xl font-bold">DSS NetWork</h2>
+              <p>Info about job</p>
+            </div>
+          </Tabs.Content>
+          <Tabs.Content anchor="tab-2">
+            <div className="flex justify-center flex-col items-center">
+              <h2 className="text-2xl font-bold">Personal Projects</h2>
+              <p>Info about my personal projects</p>
+            </div>
+          </Tabs.Content>
+        </Tabs>
+      </div>
     </section>
   );
 }
--- src/components/Studies.jsx
+++ src/components/Studies.jsx
@@ -4,7 +4,9 @@ export default function Studies() {
       <h2 className="text-5xl text-center font-bold bg-gradient-to-r from-gray-100 to-gray-700 text-transparent bg-clip-text underline-offset-8 underline decoration-2 decoration-amber-400">
         Studies
       </h2>
-      <div></div>
+      <div className="text-white flex justify-center flex-col items-center mt-10">
+        <h2 className="text-2xl font-bold text-center">Desarrollo Aplicaciones Web (DAW)</h2>
+      </div>
     </section>
   );
 }
