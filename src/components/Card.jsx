2cbf6994f828646957077685cffbfbd5d00e4727
Alvaro <alvaro.martin.crespo.00@gmail.com>
2023-06-21 11:55:21 +0200

    update links

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,10 @@ export default function Card({ name, url, description, image }) {
   return (
     <div className="max-w-sm bg-slate-900 hover:shadow-cyan-100/20 p-10 rounded-xl overflow-hidden hover:shadow-lg transition-all ease-in-out duration-200">
       <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2 flex justify-center">{name}</div>
+        <div className="flex-col items-center font-bold text-xl mb-2 flex justify-center">
+          {name}
+          <img className="rounded-xl my-5" src={image} alt={name} />
+        </div>
         <p className="text-gray-500 text-base">{description}</p>
       </div>
       <div className="flex justify-center">
