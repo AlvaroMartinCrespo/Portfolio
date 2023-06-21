1f8d451b5a7d95047a78c0924438deec06e04a76
Alvaro <alvaro.martin.crespo.00@gmail.com>
2023-06-21 10:57:13 +0200

    update skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,6 +6,7 @@ export default function Skills() {
       <h2 className="text-5xl text-center font-bold bg-gradient-to-r from-gray-100 to-gray-700 text-transparent bg-clip-text underline-offset-8 underline decoration-2 decoration-amber-400">
         Skills
       </h2>
+      <p className="text-center text-white mt-5 italic">"Passion for technology"</p>
       <div className="mt-10 md:mt-14 flex justify-center  py-10 rounded-2xl text-white mx-3">
         <div className="w-2/3 md:w-1/2 ">
           <Carousel
@@ -16,44 +17,64 @@ export default function Skills() {
             autoPlay={true}
             interval={3000}
           >
-            <div className="h-[21rem] md:h-auto">
-              <img className="w-10" src="logos/next.png" />
+            <div className="h-[21rem] md:h-auto flex justify-center">
+              <div className="md:w-2/3 ">
+                <img className="w-10" src="logos/next.png" />
+              </div>
               <p className="legend">Next.JS</p>
             </div>
-            <div className="h-[21rem] md:h-auto">
-              <img className="w-10" src="logos/html.png" />
+            <div className="h-[21rem] md:h-auto flex justify-center">
+              <div className="md:w-2/3 ">
+                <img className="w-10" src="logos/html.png" />
+              </div>
               <p className="legend">HTML</p>
             </div>
-            <div className="h-[21rem] md:h-auto">
-              <img className="w-10" src="logos/react.png" />
+            <div className="h-[21rem] md:h-auto flex justify-center">
+              <div className="md:w-2/3 ">
+                <img className="w-10" src="logos/react.png" />
+              </div>
               <p className="legend">React.Js</p>
             </div>
-            <div className="h-[21rem] md:h-auto">
-              <img className="w-10" src="logos/js.png" />
+            <div className="h-[21rem] md:h-auto flex justify-center">
+              <div className="md:w-2/3 ">
+                <img className="w-10" src="logos/js.png" />
+              </div>
               <p className="legend">Javascript</p>
             </div>
-            <div className="h-[21rem] md:h-auto">
-              <img className="w-10" src="logos/laravel.png" />
+            <div className="h-[21rem] md:h-auto flex justify-center">
+              <div className="md:w-2/3 ">
+                <img className="w-10" src="logos/laravel.png" />
+              </div>
               <p className="legend">Laravel</p>
             </div>
-            <div className="h-[21rem] md:h-auto">
-              <img className="w-10" src="logos/php.png" />
+            <div className="h-[21rem] md:h-auto flex justify-center">
+              <div className="md:w-2/3 ">
+                <img className="w-10" src="logos/php.png" />
+              </div>
               <p className="legend">Php</p>
             </div>
-            <div className="h-[21rem] md:h-auto">
-              <img className="w-10" src="logos/css.png" />
+            <div className="h-[21rem] md:h-auto flex justify-center">
+              <div className="md:w-2/3 ">
+                <img className="w-10" src="logos/css.png" />
+              </div>
               <p className="legend">CSS</p>
             </div>
-            <div className="h-[21rem] md:h-auto">
-              <img className="w-10" src="logos/tailwind.png" />
+            <div className="h-[21rem] md:h-auto flex justify-center">
+              <div className="md:w-2/3 ">
+                <img className="w-10" src="logos/tailwind.png" />
+              </div>
               <p className="legend">Tailwind</p>
             </div>
-            <div className="h-[21rem] md:h-auto">
-              <img className="w-10" src="logos/phpmyadmin.png" />
+            <div className="h-[21rem] md:h-auto flex justify-center">
+              <div className="md:w-2/3 ">
+                <img className="w-10" src="logos/phpmyadmin.png" />
+              </div>
               <p className="legend">PhpMyAdmin</p>
             </div>
-            <div className="h-[21rem] md:h-auto">
-              <img className="w-10" src="logos/bootstrap.png" />
+            <div className="h-[21rem] md:h-auto flex justify-center">
+              <div className="md:w-2/3 ">
+                <img className="w-10" src="logos/bootstrap.png" />
+              </div>
               <p className="legend">Bootstrap</p>
             </div>
           </Carousel>
