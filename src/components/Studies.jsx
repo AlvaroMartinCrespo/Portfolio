e10262eab447f410dd424b3486f7bce5186d35b3
Alvaro <alvaro.martin.crespo.00@gmail.com>
2023-06-21 11:41:04 +0200

    update studies

--- src/components/Studies.jsx
+++ src/components/Studies.jsx
@@ -7,10 +7,14 @@ export default function Studies() {
         Studies
       </h2>
       <div className="text-white flex justify-center flex-col items-center mt-10">
-        <h2 className="text-2xl font-bold text-center px-3">Desarrollo Aplicaciones Web (DAW)</h2>
+        <div className="flex justify-center items-center">
+          <span className="w-[15px] h-[15px] bg-green-500 rounded-full"></span>
+          <h2 className="text-2xl font-bold text-center px-3">Desarrollo Aplicaciones Web (DAW)</h2>
+        </div>
+
         <div className="grid grid-cols-1 items-center mt-5 px-14 text-center ">
           <div>
-            <p className="px-10 font-bold">
+            <p className="md:px-10 font-bold">
               "Desarrollar, implantar, y mantener aplicaciones web, con independencia del modelo empleado y utilizando
               tecnologías específicas, garantizando el acceso a los datos de forma segura y cumpliendo los criterios de
               accesibilidad, usabilidad y calidad exigidas en los estándares establecidos."
