e817362fcf3cd1dc57a682b4d2ae831f86a6c24f
Alvaro <alvaro.martin.crespo.00@gmail.com>
2023-11-01 11:33:01 +0100

    fix skills card

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
 import LandingImage from './components/landingImage';
 import ArrowTop from './components/arrowTop';
 import AboutMe from './components/aboutMe';
