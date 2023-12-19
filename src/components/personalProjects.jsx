9e96e9df7fc5a5492074e83f8b73a9026be40180
Alvaro <alvaro.martin.crespo.00@gmail.com>
2023-12-19 18:20:26 +0100

    update project

--- src/components/personalProjects.jsx
+++ src/components/personalProjects.jsx
@@ -14,35 +14,27 @@ export default function PersonalProjects() {
   }, []);
   return (
     <>
-      <section className="container px-auto my-0 lg:my-16">
-        <div className="flex justify-center flex-col items-center lg:flex-row gap-10 ">
+      <section className="container mx-auto my-0 lg:my-16">
+        <div className="flex flex-col lg:flex-row justify-center items-center gap-10">
           {projects
-            .filter(
-              (project) =>
-                project.name === 'howeb' ||
-                project.name === 'kahoot-app' ||
-                project.name === 'weather-api' ||
-                project.name === 'new-tech'
-            )
+            .filter((project) => ['howeb', 'kahoot-app', 'weather-api', 'tasks'].includes(project.name))
             .map((project) => (
-              <>
-                <div
-                  key={project.id}
-                  className="rounded lg:grayscale hover:grayscale-0 ease-in-out transition-all duration-300 hover:cursor-pointer"
-                >
-                  <div className="flex justify-center mb-5 flex-col">
-                    <span className="uppercase font-bold">{project.name}</span>
-                    <span>{project.description}</span>
-                  </div>
-                  <a target="_blank" rel="noreferrer" href={'https://github.com/AlvaroMartinCrespo/' + project.name}>
-                    <img
-                      className="rounded-xl hover:scale-105 transition-all ease-in-out duration-300 shadow-lg w-[50rem] h-[20rem] object-cover"
-                      src={'projects/' + project.name + '.png'}
-                      alt={project.name}
-                    />
-                  </a>
+              <div
+                key={project.id}
+                className="rounded lg:grayscale hover:grayscale-0 ease-in-out transition-all duration-300 hover:cursor-pointer"
+              >
+                <div className="flex flex-col justify-center mb-5 items-center">
+                  <span className="uppercase font-bold">{project.name}</span>
+                  <span>{project.description}</span>
                 </div>
-              </>
+                <a target="_blank" rel="noreferrer" href={'https://github.com/AlvaroMartinCrespo/' + project.name}>
+                  <img
+                    className="rounded-xl hover:scale-105 transition-all ease-in-out duration-300 shadow-lg w-full lg:w-[50rem] h-[20rem] object-cover"
+                    src={'projects/' + project.name + '.png'}
+                    alt={project.name}
+                  />
+                </a>
+              </div>
             ))}
         </div>
       </section>
