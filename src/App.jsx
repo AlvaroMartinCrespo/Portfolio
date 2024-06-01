81c527a3c32e496104975c9090a3e4b3637a6447
alvaro.martin.crespo.00 <alvaro.martin.crespo.00@gmail.com>
2024-06-01 20:55:31 +0000

    performance

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,40 @@
-import './App.css';
-import Navbar from './components/navbar';
-import LandingImage from './components/landingImage';
-import ArrowTop from './components/arrowTop';
-import AboutMe from './components/aboutMe';
-import Skills from './components/Skills';
-import Studies from './components/Studies';
-import Experience from './components/experience';
-import DownloadCV from './components/donwloadCV';
-import Contact from './components/contact';
-import Footer from './components/Footer';
-import Analiticas from './components/Analiticas';
+import React, { lazy, Suspense } from 'react';
+const Navbar = lazy(() => import('./components/navbar'));
+const LandingImage = lazy(() => import('./components/landingImage'));
+const ArrowTop = lazy(() => import('./components/arrowTop'));
+const AboutMe = lazy(() => import('./components/aboutMe'));
+const Skills = lazy(() => import('./components/Skills'));
+const Studies = lazy(() => import('./components/Studies'));
+const Experience = lazy(() => import('./components/experience'));
+const DownloadCV = lazy(() => import('./components/donwloadCV'));
+const Contact = lazy(() => import('./components/contact'));
+const Footer = lazy(() => import('./components/Footer'));
+const Analiticas = lazy(() => import('./components/Analiticas'));
+
 
 function App() {
   return (
     <>
-      <Analiticas />
-      <ArrowTop />
-      <Navbar />
-      <LandingImage />
-      {/* Page */}
-      <span id="about"></span>
-      <AboutMe />
-      <span id="skills"></span>
-      <Skills />
-      <span id="studies"></span>
-      <Studies />
-      <span id="experience"></span>
-      <Experience />
-      <span id="cv"></span>
-      <DownloadCV />
-      <span id="contact"></span>
-      <Contact />
-      <Footer />
+      <Suspense fallback={<div>Loading...</div>}> 
+        <Analiticas />
+        <ArrowTop />
+        <Navbar />
+        <LandingImage />
+        {/* Page */}
+        <span id="about"></span>
+        <AboutMe />
+        <span id="skills"></span>
+        <Skills />
+        <span id="studies"></span>
+        <Studies />
+        <span id="experience"></span>
+        <Experience />
+        <span id="cv"></span>
+        <DownloadCV />
+        <span id="contact"></span>
+        <Contact />
+        {/* ... rest of your components */}
+      </Suspense>
     </>
   );
 }
